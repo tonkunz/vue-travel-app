23b54f03def8be5f0ef7f65c6bffab5a8deb80c9
Everton Kunz <ehokeverton@gmail.com>
2020-11-06 00:08:32 -0400

    First commit

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,13 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 
+/** Our pages */
+import Brazil from "../views/Brazil.vue";
+import Havaii from "../views/Havaii.vue";
+import Panama from "../views/Panama.vue";
+import Jamaica from "../views/Jamaica.vue";
+
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -18,7 +25,27 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  },
+  {
+    path: "/brazil",
+    name: "brazil",
+    component: Brazil
+  },
+  {
+    path: "/havaii",
+    name: "havaii",
+    component: Havaii,
+  },
+  {
+    path: "/panama",
+    name: "panama",
+    component: Panama,
+  },
+  {
+    path: "/jamaica",
+    name: "jamaica",
+    component: Jamaica,
+  },
 ];
 
 const router = new VueRouter({
